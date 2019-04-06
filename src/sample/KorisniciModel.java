96fd33e8e68a959830baa7eb0223b8ee37157685
HARUN <harun.hadzic17@gmail.com>
2019-04-06 10:13:49 +0200

    Podloga za povezivanje

--- src/sample/KorisniciModel.java
+++ src/sample/KorisniciModel.java
@@ -1,6 +1,38 @@
 package sample;
 
-public class KorisniciModel {
+import javafx.beans.property.SimpleObjectProperty;
 
+import javafx.collections.FXCollections;
+
+import javafx.collections.ObservableList;
+
+public class KorisniciModel {
+    private ObservableList<Korisnik> korisnik = FXCollections.observableArrayList();
+    private SimpleObjectProperty trenutniKorisnik = new SimpleObjectProperty();
+
+    public ObservableList<Korisnik> getKorisnik() {
+        return korisnik;
+    }
+
+    public void setKorisnik(ObservableList<Korisnik> korisnik) {
+        this.korisnik = korisnik;
+    }
+
+    public Object getTrenutniKorisnik() {
+        return trenutniKorisnik.get();
+    }
+
+    public SimpleObjectProperty trenutniKorisnikProperty() {
+        return trenutniKorisnik;
+    }
+
+    public void setTrenutniKorisnik(Object trenutniKorisnik) {
+        this.trenutniKorisnik.set(trenutniKorisnik);
+    }
+
+    void napuni (){
+        korisnik.add(new Korisnik( "Vedran" , "Ljubovic" , " vljubovic1@etf.unsa.ba" , "vljubovic1", "password"));
+        korisnik.add(new Korisnik( "Tarik" , "Sijarcic" , " tsijarcic1@etf.unsa.ba" , "tare", "passw"));
+    }
 
 }
--- src/sample/Korisnik.java
+++ src/sample/Korisnik.java
@@ -1,7 +1,88 @@
 package sample;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Korisnik {
 
+    private SimpleStringProperty ime;
+    private SimpleStringProperty prezime;
+    private SimpleStringProperty mail;
+    private SimpleStringProperty korisnickoIme;
+    private SimpleStringProperty lozinka;
 
+    public String getIme() {
+        return ime.get();
+    }
 
+    public SimpleStringProperty imeProperty() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime.set(ime);
+    }
+
+    public String getPrezime() {
+        return prezime.get();
+    }
+
+    public SimpleStringProperty prezimeProperty() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime.set(prezime);
+    }
+
+    public String getMail() {
+        return mail.get();
+    }
+
+    public SimpleStringProperty mailProperty() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail.set(mail);
+    }
+
+    public String getKorisnickoIme() {
+        return korisnickoIme.get();
+    }
+
+    public SimpleStringProperty korisnickoImeProperty() {
+        return korisnickoIme;
+    }
+
+    public void setKorisnickoIme(String korisnickoIme) {
+        this.korisnickoIme.set(korisnickoIme);
+    }
+
+    public String getLozinka() {
+        return lozinka.get();
+    }
+
+    public SimpleStringProperty lozinkaProperty() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka.set(lozinka);
+    }
+
+    public Korisnik(){};
+
+    public Korisnik(String ime, String prezime, String mail,
+                    String korisnickoIme, String lozinka) {
+        this.ime = new SimpleStringProperty(ime);
+        this.prezime = new SimpleStringProperty(prezime);
+        this.mail = new SimpleStringProperty(mail);
+        this.korisnickoIme = new SimpleStringProperty(korisnickoIme);
+        this.lozinka = new SimpleStringProperty(lozinka);
+    }
+
+    @Override
+    public String toString() {
+        return ime.get() + " " + prezime.get();
+    }
 }
